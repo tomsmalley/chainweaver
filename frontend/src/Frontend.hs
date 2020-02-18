0481549de07c7b9b8e77bd7955f1f5a112639227
Matthew Bauer <mjbauer95@gmail.com>
2020-02-18 16:47:43 -0500

    Move everything to Frontend module
    Also remove unneeded stuff

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -2,103 +2,83 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE QuasiQuotes #-}
+
 module Frontend where
 
-import           Control.Monad            (void)
-import           Data.Text                (Text)
-import qualified Data.Text                as T
 import           Reflex.Dom.Core
 import Pact.Server.ApiV1Client (runTransactionLoggerT, logTransactionStdout)
 
 import           Obelisk.Frontend
 import           Obelisk.Route.Frontend
-import           Obelisk.Generated.Static
 
-import           Common.Api
 import           Common.Route
 import           Frontend.Crypto.Browser
-import           Frontend.ReplGhcjs
 import           Frontend.Storage
+
+import Control.Monad.State.Strict
+import Data.Aeson (FromJSON, ToJSON)
+import Data.Text (Text)
+import Language.Javascript.JSaddle (liftJSM)
+import Obelisk.Route (R)
+
+import Frontend.Crypto.Class
+import qualified Frontend.Store as Store
+
+import qualified Servant.Client.JSaddle            as S
+
+import Data.ByteString (ByteString)
+import qualified Data.Text.Encoding as T
+import Safe (fromJustNote)
+import Pact.Server.ApiV1Client as Pact
+import qualified Pact.Types.Command as Pact
+import qualified Pact.Types.Hash as Pact
+import qualified Text.URI as URI hiding (uriPath)
+import qualified Text.URI.QQ as URI
+import Text.URI.Lens (uriPath)
+
+app
+  :: forall key t m.
+     ( MonadWidget t m
+     , HasStorage m
+     , HasCrypto key m
+     , FromJSON key, ToJSON key
+     )
+  => RoutedT t (R FrontendRoute) m ()
+app = do
+  _ <- _storageVersioner_upgrade $ Store.versioner @key
+
+  let
+    args :: [Text]
+    args = ["chainweb", "0.0", "testnet04", "chain", "8", "pact"]
+    path :: [URI.RText 'URI.PathPiece]
+    path = fromJustNote "Building chain base path failed!" . traverse URI.mkPathPiece $ args
+    host :: URI.RText 'URI.Host
+    host = fromJustNote "HOOOST" $ URI.mkHost "eu1.testnet.chainweb.com"
+    auth :: URI.Authority
+    auth = URI.Authority Nothing host Nothing
+    uri :: URI.URI
+    uri = URI.URI (Just [URI.scheme|https|]) (Right auth) Nothing [] Nothing
+      & uriPath .~ path
+
+    payload :: ByteString
+    payload = "{\"networkId\":\"testnet\",\"payload\":{\"exec\":{\"data\":{},\"code\":\"(list-modules)\"}},\"signers\":[],\"meta\":{\"creationTime\":1580337414,\"ttl\":28800,\"gasLimit\":600,\"chainId\":\"0\",\"gasPrice\":1.0e-5,\"sender\":\"5d0e99e446a078a356e86934c4b1d223f482e9f8888fb215502d3543b4abfdcf\"},\"nonce\":\"2020-01-29 22:37:09.307237957 UTC\"}"
+
+  liftIO $ do
+    putStrLn "=============================="
+  r <- case S.parseBaseUrl $ URI.renderStr uri of
+    Nothing -> error "nope"
+    Just baseUrl -> do
+       let env = S.mkClientEnv baseUrl
+           cmd = fmap T.decodeUtf8 $ Pact.Command payload [] $ Pact.hash payload
+       liftJSM $ flip S.runClientM env $ Pact.local apiV1Client cmd
+  liftIO $ do
+    putStrLn "=============================="
+    print r
 
 frontend :: Frontend (R FrontendRoute)
 frontend = Frontend
-  { _frontend_head = do
-      let backendEncoder = either (error "frontend: Failed to check backendRouteEncoder") id $
-            checkEncoder backendRouteEncoder
-      -- Global site tag (gtag.js) - Google Analytics
-      gaTrackingId <- maybe "UA-127512784-1" T.strip <$> getTextCfg "frontend/tracking-id"
-      let
-        gtagSrc = "https://www.googletagmanager.com/gtag/js?id=" <> gaTrackingId
-      elAttr "script" ("async" =: "" <> "src" =: gtagSrc) blank
-      el "script" $ text $ T.unlines
-        [ "window.dataLayer = window.dataLayer || [];"
-        , "function gtag(){dataLayer.push(arguments);}"
-        , "gtag('js', new Date());"
-        , "gtag('config', '" <> gaTrackingId <> "');"
-        ]
-
-      base <- getConfigRoute
-      _ <- newHead $ \r -> base <> renderBackendRoute backendEncoder r
-      pure ()
-
-  , _frontend_body = prerender_ loaderMarkup $ do
+  { _frontend_head = blank
+  , _frontend_body = prerender_ blank $ do
     mapRoutedT (flip runTransactionLoggerT logTransactionStdout . runBrowserStorageT . runBrowserCryptoT) app
   }
-
-tshow :: Show a => a -> Text
-tshow = T.pack . show
-
-loaderMarkup :: DomBuilder t m => m ()
-loaderMarkup = divClass "spinner" $ do
-  divClass "spinner__cubes" $ do
-    divClass "cube1" blank
-    divClass "cube2" blank
-  divClass "spinner__msg" $ text "Loading"
-
-newHead :: (Prerender js t m, DomBuilder t m) => (R BackendRoute -> Text) -> m (Event t ())
-newHead routeText = do
-  el "title" $ text "Kadena - Pact Testnet"
-  elAttr "link" ("rel" =: "icon" <> "type" =: "image/png" <> "href" =: static @"img/favicon/favicon-96x96.png") blank
-  meta ("name" =: "description" <> "content" =: "Write, test, and deploy safe smart contracts using Pact, Kadena's programming language")
-  meta ("name" =: "keywords" <> "content" =: "kadena, pact, pact testnet, pact language, pact programming language, smart contracts, safe smart contracts, smart contract language, blockchain, learn blockchain programming, chainweb")
-  meta ("charset" =: "utf-8")
-  meta ("name" =: "google" <> "content" =: "notranslate")
-  meta ("http-equiv" =: "Content-Language" <> "content" =: "en_US")
-  elAttr "link" ("href" =: routeText (BackendRoute_Css :/ ()) <> "rel" =: "stylesheet") blank
-  elAttr "style" ("type" =: "text/css") $ text haskellCss
-
-  ss "https://fonts.googleapis.com/css?family=Roboto"
-  ss "https://fonts.googleapis.com/css?family=Work+Sans"
-  ss (static @"css/font-awesome.min.css")
-  ss (static @"css/ace-theme-chainweaver.css")
-  js "/static/js/ace/ace.js"
-  prerender_ blank $ js "/static/js/ace/mode-pact.js"
-  js (static @"js/nacl-fast.min-v1.0.0.js")
-  (bowser, _) <- js' (static @"js/bowser.min.js")
-  pure $ domEvent Load bowser
-  where
-    js :: forall t n. DomBuilder t n => Text -> n ()
-    js = void . js'
-    js' :: forall t n. DomBuilder t n => Text -> n (Element EventResult (DomBuilderSpace n) t, ())
-    js' url = elAttr' "script" ("type" =: "text/javascript" <> "src" =: url <> "charset" =: "utf-8") blank
-    ss url = elAttr "link" ("href" =: url <> "rel" =: "stylesheet") blank
-    meta attrs = elAttr "meta" attrs blank
-
-    -- Allows the use of `static` in CSS and sharing parameters with desktop apps
-    haskellCss = T.unlines
-      [ "body { min-width: " <> tshow w <> "px; " <> "min-height: " <> tshow h <> "px; }"
-      , alertImg ".icon_type_error"                $ static @"img/error.svg"
-      , alertImg ".icon_type_warning"              $ static @"img/warning.svg"
-      , alertImg "div.ace_gutter-cell.ace_error"   $ static @"img/error.svg"
-      , alertImg "div.ace_gutter-cell.ace_warning" $ static @"img/warning.svg"
-      ]
-      where
-        bgImg src = "background-image: url(" <> src <> "); background-position: left center"
-        alertImg sel src = sel <> " { " <> bgImg src <> " } ";
-        (w,h) = minWindowSize
-
-minWindowSize :: (Int, Int)
-minWindowSize = (800, 600)
-
-defaultWindowSize :: (Int, Int)
-defaultWindowSize = (1400, 900)
