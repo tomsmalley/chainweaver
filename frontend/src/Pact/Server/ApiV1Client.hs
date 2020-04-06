9027762f123cedac644ddcbecf417ab664029c4d
Leon P Smith <leon.smith@obsidian.systems>
2020-04-06 13:34:59 -0400

    Shrink

--- frontend/src/Pact/Server/ApiV1Client.hs
+++ frontend/src/Pact/Server/ApiV1Client.hs
@@ -8,27 +8,22 @@
 module Pact.Server.ApiV1Client
   ( ApiV1Client(local)
   , apiV1Client
-  , HasTransactionLogger
+  , HasTransactionLogger()
   , runTransactionLoggerT
   , logTransactionStdout
   ) where
 
-import Control.Lens
 import Control.Monad.IO.Class
 import Control.Monad.Primitive
 import Control.Monad.Reader hiding (local)
 import Control.Monad.Ref
-import Data.Aeson (ToJSON, FromJSON)
 import Data.Coerce
-import Data.Foldable (for_)
 import Data.Proxy
 import Data.Text (Text)
-import Data.Time (UTCTime, getCurrentTime)
 import Language.Javascript.JSaddle (MonadJSM)
 import Obelisk.Configs
 import Obelisk.Route.Frontend
 import Pact.Server.API
-import Pact.Types.API
 import Pact.Types.Command
 import Pact.Types.Hash (Hash)
 import Reflex.Dom.Core
@@ -36,86 +31,24 @@ import Reflex.Host.Class (MonadReflexCreateTrigger)
 import Servant.API
 import Servant.Client.Core hiding (Client)
 import Servant.Client.JSaddle hiding (Client)
-import qualified Data.Aeson as Aeson
-import qualified Data.Aeson.Types as Aeson
 import qualified Data.Aeson.Text as Aeson
-import qualified Data.Text as T
 import qualified Data.Text.Lazy.IO as LT
 import qualified Control.Monad.Reader as Reader
 
-type TransactionLogger = CommandLog -> IO ()
+type TransactionLogger = () -> IO ()
 
 data ApiV1Client m = ApiV1Client
-  { send :: TransactionLogger -> SubmitBatch -> m RequestKeys
-  , poll :: Poll -> m PollResponses
-  , listen :: ListenerRequest -> m ListenResponse
-  , local :: Command Text -> m (CommandResult Hash)
+  { local :: Command Text -> m (CommandResult Hash)
   }
 
 {- apiV1API :: Proxy ApiV1API -}
 {- apiV1API = Proxy -}
 apiV1Client :: forall m. (MonadIO m, MonadReader ClientEnv m, RunClient m) => ApiV1Client m
 apiV1Client = ApiV1Client
-  { send = \appendLog batch@(SubmitBatch commands) -> do
-      url <- asks baseUrl
-      timestamp <- liftIO getCurrentTime
-      for_ commands $ \command -> liftIO $ appendLog $ CommandLog
-        { _commandLog_command = command
-        , _commandLog_timestamp = timestamp
-        , _commandLog_url = T.pack $ showBaseUrl url
-        }
-      sendF batch
-  , poll = pollF
-  , listen = listenF
-  , local = localF
+  { local = localF
   }
   where
-    sendF :<|> pollF :<|> listenF :<|> localF = clientIn apiV1API (Proxy :: Proxy m)
-
--- | Commands are logged with the time they were sent and the node URL they were
--- sent to. We only define 'toJSON' for the current version, thus only write
--- logs in the latest version, but we allow parsing older versions automatically
--- with 'commandLogParsers'.
-data CommandLog = CommandLog
-  { _commandLog_timestamp :: UTCTime
-  , _commandLog_url :: Text
-  , _commandLog_command :: Command Text
-  } deriving (Eq, Show)
-
-instance ToJSON CommandLog where
-  toJSON cl = Aeson.object
-    [ "version" Aeson..= commandLogCurrentVersion
-    , "timestamp" Aeson..= _commandLog_timestamp cl
-    , "url" Aeson..= _commandLog_url cl
-    , "command" Aeson..= _commandLog_command cl
-    ]
-
-instance FromJSON CommandLog where
-  parseJSON = Aeson.withObject "CommandLog" $ \o -> do
-    version <- o Aeson..: "version"
-    case commandLogParsers ^? ix version of
-      Nothing -> fail $ "Unexpected version: " <> show version
-      Just parse -> parse o
-
-commandLogCurrentVersion :: Int
-commandLogCurrentVersion = pred $ length commandLogParsers
-
--- | Add newer versions to the end of this list
-commandLogParsers :: [Aeson.Object -> Aeson.Parser CommandLog]
-commandLogParsers =
-  [ parseCommandLogV0
-  ]
-
-parseCommandLogV0 :: Aeson.Object -> Aeson.Parser CommandLog
-parseCommandLogV0 o = do
-  timestamp <- o Aeson..: "timestamp"
-  url <- o Aeson..: "url"
-  command <- o Aeson..: "command"
-  pure $ CommandLog
-    { _commandLog_timestamp = timestamp
-    , _commandLog_url = url
-    , _commandLog_command = command
-    }
+    _sendF :<|> _pollF :<|> _listenF :<|> localF = clientIn apiV1API (Proxy :: Proxy m)
 
 class HasTransactionLogger m where
   askTransactionLogger :: m TransactionLogger
@@ -190,6 +123,3 @@ runTransactionLoggerT = runReaderT . unTransactionLoggerT
 
 logTransactionStdout :: TransactionLogger
 logTransactionStdout = LT.putStrLn . Aeson.encodeToLazyText
-
-logTransactionFile :: FilePath -> CommandLog -> IO ()
-logTransactionFile f = LT.appendFile f . (<> "\n") . Aeson.encodeToLazyText
