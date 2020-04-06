4f12336d3eaa725b60ddc05c8a029df687b6a253
Leon P Smith <leon.smith@obsidian.systems>
2020-04-06 18:37:30 -0400

    Shrink!

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -12,14 +12,18 @@ import           Obelisk.Frontend
 import           Obelisk.Route.Frontend
 
 import           Common.Route
-import Frontend.Storage.Class (HasStorage, backupLocalStorage, StoreKeyMetaPrefix(..))
-import Frontend.Storage.Browser (runBrowserStorageT)
 
 import Language.Javascript.JSaddle (liftJSM)
 import Obelisk.Route (R)
 
 import qualified Servant.Client.JSaddle            as S
 
+import qualified Data.Aeson as Aeson
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Dependent.Map as DMap
+import Data.Dependent.Sum.Orphans ()
+import Data.Functor.Identity (Identity)
+import Data.Text (Text)
 import qualified Data.Text.Encoding as T
 import Pact.Server.ApiV1Client as Pact
 import qualified Pact.Types.Command as Pact
@@ -30,13 +34,15 @@ import qualified Frontend.Store.V0 as V0 (StoreFrontend)
 import Frontend.Crypto.Ed25519
     ( PrivateKey )
 
+encodeText :: Aeson.ToJSON a => a -> Text
+encodeText = T.decodeUtf8 . BL.toStrict . Aeson.encode
+
 app :: forall t m.
      ( MonadWidget t m
-     , HasStorage m
      )
   => RoutedT t (R FrontendRoute) m ()
 app = do
-  _ <- backupLocalStorage @(V0.StoreFrontend PrivateKey) (StoreKeyMetaPrefix "StoreFrontend_Meta") 0
+  encodeText (DMap.empty :: DMap.DMap (V0.StoreFrontend PrivateKey) Identity) `seq` pure ()
 
   let env = S.mkClientEnv $ S.BaseUrl S.Https "eu1.testnet.chainweb.com" 80 "/chainweb/0.0/testnet04/chain/8/pact"
       cmd = fmap T.decodeUtf8 $ Pact.Command "" [] $ Pact.hash ""
@@ -48,6 +54,5 @@ app = do
 frontend :: Frontend (R FrontendRoute)
 frontend = Frontend
   { _frontend_head = blank
-  , _frontend_body = prerender_ blank $ do
-    mapRoutedT runBrowserStorageT app
+  , _frontend_body = prerender_ blank app
   }
