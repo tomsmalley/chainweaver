394ea7fc689b4a3fec01cf7be3985733f167cabd
Leon P Smith <leon.smith@obsidian.systems>
2020-04-06 17:09:41 -0400

    Minor cleanup

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -12,13 +12,11 @@ import           Obelisk.Frontend
 import           Obelisk.Route.Frontend
 
 import           Common.Route
-import Frontend.Storage.Class (HasStorage)
+import Frontend.Storage.Class (HasStorage, backupLocalStorage, StoreKeyMetaPrefix(..))
 import Frontend.Storage.Browser (runBrowserStorageT)
 
 import Language.Javascript.JSaddle (liftJSM)
 import Obelisk.Route (R)
-
-import qualified Frontend.Store as Store
 
 import qualified Servant.Client.JSaddle            as S
 
@@ -26,6 +24,9 @@ import qualified Data.Text.Encoding as T
 import Pact.Server.ApiV1Client as Pact
 import qualified Pact.Types.Command as Pact
 import qualified Pact.Types.Hash as Pact
+
+import Data.Proxy (Proxy(Proxy))
+import qualified Frontend.Store.V0 as V0 (StoreFrontend)
 
 import Frontend.Crypto.Ed25519
     ( PrivateKey )
@@ -36,7 +37,7 @@ app :: forall t m.
      )
   => RoutedT t (R FrontendRoute) m ()
 app = do
-  _ <- Store.upgrade @PrivateKey
+  _ <- backupLocalStorage (StoreKeyMetaPrefix "StoreFrontend_Meta") (Proxy @(V0.StoreFrontend PrivateKey)) 0
 
   let env = S.mkClientEnv $ S.BaseUrl S.Https "eu1.testnet.chainweb.com" 80 "/chainweb/0.0/testnet04/chain/8/pact"
       cmd = fmap T.decodeUtf8 $ Pact.Command "" [] $ Pact.hash ""
